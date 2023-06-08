5c2c89ab468fa2f70ace7f465cfded64842865df
Yordan Dobrev <yordan.dobrev@amusnet.com>
2023-06-08 20:06:13 +0300

    feat: setup main react client

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { Component, ReactNode } from 'react'
+
+type State = { 
+}
+
+export default class App extends Component<any, State> {
+  constructor() {
+    super({})
+    this.state = {
+    }
+  }
+
+  render(): ReactNode {
+    return (
+      <>
+      </>
+    )
+  }
+}
